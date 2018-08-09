a547b18fe76dc9af3c30eda6be3ab84d0a3f6921
Ryan Foss1 <Ryan_Foss@anfcorp.com>
2018-08-09 17:29:52 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,37 +1,70 @@
 $(document).ready(() => {
-    $("#searchForm").on('submit', (e) => {
-        let searchText = $("#searchTerm").val();
-        getMovies(searchText);
-        e.preventDefault();
-    });
+  $("#searchForm").on('submit', (e) => {
+      let search = 's=' + $("#searchTerm").val();
+      getMovies(search);
+      e.preventDefault();
+  });
+
+  if (moviesArr) {
+    search = moviesArr.split(',');
+    search =  'i=' + search[0];
+    getMovies(search);
+  }
+
 });
 
 function save(movie){
   let id = movie.id;
   $(`#${id}`).text('Saved to list!');
 }
+function remove(results){
+  let id = results.id;
+  $(`#${id}`).text('Removed from list!');
+}
 
-function getMovies(searchText) {
-  axios.get('http://www.omdbapi.com?s=' + searchText + '&apikey=72483cf1')
+function getMovies(search) {
+
+  axios.get('http://www.omdbapi.com?' + search + '&apikey=72483cf1')
     .then((response) => {
-      let results = response.data.Search;      
-      let output = '';
+      let output = '';      
+      let results = '';
 
-      $.each(results, (index, movie) => {
+      if (search.includes('s=')){
+        results = response.data.Search;
+        $.each(results, (index, movie) => {
+          output += `
+            <div class="movie">
+              <a href='https://www.imdb.com/title/${movie.imdbID}' target="_blank"><img src='${movie.Poster}'></a>
+              <h1><a href='https://www.imdb.com/title/${movie.imdbID}' target="_blank">${movie.Title}</a></h1>
+              <form action="/" method="POST">
+                  <button type="submit" id='${movie.imdbID}' name='movie' value='${movie.imdbID}' onClick='save(${movie.imdbID})'>
+                    <i class="fas fa-star"></i>
+                  </button>
+              </form>
+              <p id='${movie.imdbID}'></p>
+            </div>
+          `;
+        })        
+      } else {
+        results = response.data;
+
         output += `
           <div class="movie">
-            <a href='https://www.imdb.com/title/${movie.imdbID}' target="_blank"><img src='${movie.Poster}'></a>
-            <h1><a href='https://www.imdb.com/title/${movie.imdbID}' target="_blank">${movie.Title}</a></h1>
+            <a href='https://www.imdb.com/title/${results.imdbID}' target="_blank"><img src='${results.Poster}'></a>
+            <h1><a href='https://www.imdb.com/title/${results.imdbID}' target="_blank">${results.Title}</a></h1>
             <form action="/" method="POST">
-                <button type="submit" id='${movie.imdbID}' name='movie' value='${movie.imdbID}' onClick='save(${movie.imdbID})'>
-                  <i class="fas fa-star"></i>
+                <button type="submit" id='${results.imdbID}' name='${listArr}' value='${results.imdbID}' onClick='remove(${results.imdbID})'>
+                  <i class="fas fa-minus"></i>
                 </button>
+                <p id='activeList' name='activeList' value='${listArr}' >${listArr}</p>
             </form>
-            <p id='${movie.imdbID}'></p>
+            <p id='${results.imdbID}'></p>
           </div>
         `;
-      })
+      }
 
+
+    
       $('#output').html(output);
     }).catch((err) => {
       console.log(err);
--- server.js
+++ server.js
@@ -18,13 +18,19 @@ app.use(express.static(path.join(__dirname)));
 app.set('view engine', 'hbs');
 
 let listArr = [];
+let moviesArr = [];
+let activeList = '';
 
 // routes
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   List.distinct('title').then((lists) => {
-  	listArr = [];
+  	listArr = [];	
     listArr.push(lists);     
-  	res.render('index.hbs', { listArr: lists });
+  	res.render('index.hbs', { 
+  		listArr: lists,
+  		moviesArr: moviesArr[0],
+ 			activeList: activeList 		
+  	});	
   });  
 });
 
@@ -32,7 +38,7 @@ app.get('/home', (req, res) => {
 app.post('/', (req, res) => {
 
 	if (req.body.createList){
-		listArr.push(req.body.createList);
+		listArr.push(req.body.createList); 
 
 	  var list = new List({
 	    title: req.body.createList
@@ -41,23 +47,35 @@ app.post('/', (req, res) => {
 	  }, (e) => {
 	    res.status(400).send(e);
 	  });
-
 		res.redirect('back');
 
 	} else if (req.body.aListItem) {
 
-
 	  List.find({'title': req.body.aListItem}).then((lists) => {
-	  	console.log(lists[0].items);
-
-// create var and pass to js to use in getMovies()
-// replace star with minus
-
-
+			activeList = req.body.aListItem;   	
+			moviesArr = [];  		  	
+		  moviesArr.push(lists[0].items); 
 
    	}, (e) => {
       res.status(400).send(e);
     });  
+		res.redirect('back');
+
+	} else if (req.body) {
+	  List.find({'title': Object.keys(req.body)[0]}).then((lists) => {
+	  	let test = Object.keys(req.body)[0];
+	  	test = req.body.test;
+			List.update(
+			  { },
+			  { $pull: { items: { test } } },
+			  { multi: true }
+			)
+			console.log(req.body.test);
+
+   	}, (e) => {
+      res.status(400).send(e);
+    });  
+		res.redirect('back');
 
 	} else {
     List.update(
