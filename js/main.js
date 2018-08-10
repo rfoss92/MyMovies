3523032af66e7fed951786ff48329536058ae882
Ryan Foss1 <Ryan_Foss@anfcorp.com>
2018-08-10 16:35:42 -0400

    updates

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
 $(document).ready(() => {
   $("#searchForm").on('submit', (e) => {
-      let search = 's=' + $("#searchTerm").val();
-      getMovies(search);
-      e.preventDefault();
-  });
-
-  if (moviesArr) {
-    search = moviesArr.split(',');
-    search =  'i=' + search[0];
+    let search = 's=' + $("#searchTerm").val();
     getMovies(search);
+    e.preventDefault();
+  });
+  
+  if (listArr) {
+    listArrDropDown = listArr.split(',');
+    for (var i = 0; i < listArrDropDown.length; i++){
+      listArrDropDown[i] = '<a>' + listArrDropDown[i] + '</a>';
+    }
+    listArrDropDown = listArrDropDown.join('');
   }
 
+  if (moviesArr) {
+    moviesArr = moviesArr.split(',');
+    getMovies(moviesArr);
+  }
 });
 
 function save(movie){
@@ -23,50 +29,78 @@ function remove(results){
 }
 
 function getMovies(search) {
+  let output = '';
+  if (search.constructor === Array){
+    for (var i = 0; i < search.length; i++) {
+      search[i] = 'i=' + search[i]; 
+      axios.get('http://www.omdbapi.com?' + search[i] + '&apikey=72483cf1')
+        .then((response) => {   
+          let results = response.data;
+          output += `
+            <div class="movie">
+              <a href='https://www.imdb.com/title/${results.imdbID}' target="_blank"><img src='${results.Poster}'></a>
+              <h2><a href='https://www.imdb.com/title/${results.imdbID}' target="_blank">${results.Title}</a></h2>
+              <form action="/" method="POST">
+                  <button type="submit" id='${results.imdbID}' name='${listArr}' value='${results.imdbID}' onClick='remove(${results.imdbID})'>
+                    <i class="fas fa-minus"></i>
+                  </button>
+              </form>
+              <p id='${results.imdbID}'></p>
+            </div>
+          `;
+        $('#output').html(output);
+        }).catch((err) => {
+          console.log(err);
+        });
+    }
 
-  axios.get('http://www.omdbapi.com?' + search + '&apikey=72483cf1')
-    .then((response) => {
-      let output = '';      
-      let results = '';
-
-      if (search.includes('s=')){
-        results = response.data.Search;
+  } else {
+    axios.get('http://www.omdbapi.com?' + search + '&apikey=72483cf1')
+      .then((response) => {   
+        let results = response.data.Search;
         $.each(results, (index, movie) => {
           output += `
             <div class="movie">
               <a href='https://www.imdb.com/title/${movie.imdbID}' target="_blank"><img src='${movie.Poster}'></a>
-              <h1><a href='https://www.imdb.com/title/${movie.imdbID}' target="_blank">${movie.Title}</a></h1>
+              <h2><a href='https://www.imdb.com/title/${movie.imdbID}' target="_blank">${movie.Title}</a></h2>
               <form action="/" method="POST">
-                  <button type="submit" id='${movie.imdbID}' name='movie' value='${movie.imdbID}' onClick='save(${movie.imdbID})'>
+                  <button type="submit" id='${movie.imdbID}' name='movie' value='${movie.imdbID}, 5' onClick='save(${movie.imdbID})'>
                     <i class="fas fa-star"></i>
                   </button>
+                  
+                  <div class="dropdown">
+                    <button onclick="myFunction()" class="dropbtn">Select List</button>
+                    <div id="myDropdown" class="dropdown-content">
+                      ${listArrDropDown}
+                    </div>
+                  </div>
+
               </form>
               <p id='${movie.imdbID}'></p>
             </div>
           `;
-        })        
-      } else {
-        results = response.data;
+        })   
+      $('#output').html(output);   
+      }).catch((err) => {
+        console.log(err);
+      });
+  }
+}
 
-        output += `
-          <div class="movie">
-            <a href='https://www.imdb.com/title/${results.imdbID}' target="_blank"><img src='${results.Poster}'></a>
-            <h1><a href='https://www.imdb.com/title/${results.imdbID}' target="_blank">${results.Title}</a></h1>
-            <form action="/" method="POST">
-                <button type="submit" id='${results.imdbID}' name='${listArr}' value='${results.imdbID}' onClick='remove(${results.imdbID})'>
-                  <i class="fas fa-minus"></i>
-                </button>
-                <p id='activeList' name='activeList' value='${listArr}' >${listArr}</p>
-            </form>
-            <p id='${results.imdbID}'></p>
-          </div>
-        `;
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
-
-
-    
-      $('#output').html(output);
-    }).catch((err) => {
-      console.log(err);
-    });
+    }
+  }
 }
--- server.js
+++ server.js
@@ -3,20 +3,18 @@ const path = require('path');
 const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const hbs = require('hbs');
-
-// mongo
-const {ObjectID} = require('mongodb');
-var {mongoose} = require('./server/mongoose');
-var {List} = require('./server/models/list');
+let {mongoose} = require('./server/mongoose');
+let {List} = require('./server/models/list');
 
 // config
-var app = express();
+let app = express();
 app.listen(port, () => console.log(`Server is up on ${port}`));
-app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname)));
 app.set('view engine', 'hbs');
 
+// variables
 let listArr = [];
 let moviesArr = [];
 let activeList = '';
@@ -24,12 +22,12 @@ let activeList = '';
 // routes
 app.get('/', (req, res) => {
   List.distinct('title').then((lists) => {
-  	listArr = [];	
-    listArr.push(lists);     
+  	listArr = [lists];
+  	activeList = listArr[0][0];	   
   	res.render('index.hbs', { 
   		listArr: lists,
-  		moviesArr: moviesArr[0],
- 			activeList: activeList 		
+  		moviesArr,
+ 			activeList 		
   	});	
   });  
 });
@@ -37,56 +35,41 @@ app.get('/', (req, res) => {
 // post
 app.post('/', (req, res) => {
 
-	if (req.body.createList){
+	// create list
+	if (req.body.createList) {
 		listArr.push(req.body.createList); 
-
-	  var list = new List({
+	  let list = new List({
 	    title: req.body.createList
-	  });
-	  list.save().then((doc) => {
-	  }, (e) => {
-	    res.status(400).send(e);
-	  });
+	  }).save();
 		res.redirect('back');
 
+	// retrieve list
 	} else if (req.body.aListItem) {
-
 	  List.find({'title': req.body.aListItem}).then((lists) => {
-			activeList = req.body.aListItem;   	
-			moviesArr = [];  		  	
-		  moviesArr.push(lists[0].items); 
-
-   	}, (e) => {
-      res.status(400).send(e);
-    });  
+			activeList = req.body.aListItem;
+			moviesArr = [lists[0].items];
+   	});
 		res.redirect('back');
 
+	// add to list
+	} else if (req.body.movie) {
+    List.updateOne(
+    	{ 'title': activeList},
+    	{ $addToSet: { 'items': [req.body.movie] } },
+    	{ upsert: true }
+    ).then();
+    console.log(req.body);
+
+  // remove from list
 	} else if (req.body) {
-	  List.find({'title': Object.keys(req.body)[0]}).then((lists) => {
-	  	let test = Object.keys(req.body)[0];
-	  	test = req.body.test;
-			List.update(
-			  { },
-			  { $pull: { items: { test } } },
-			  { multi: true }
-			)
-			console.log(req.body.test);
-
-   	}, (e) => {
-      res.status(400).send(e);
-    });  
-		res.redirect('back');
-
-	} else {
-    List.update(
-    	{ 'title': listArr[0]}, 
-    	{ $push: { 'items': [req.body.movie] } }
-    ).then((lists) => {
-      List.find({title: 'items'}).then((lists) => {
-      }, (e) => {
-        res.status(400).send(e);
-      });
-    });
+		for (var key in req.body) {}
+	  List.update(
+		  { 'title': activeList },
+		  { $pull: {'items' : req.body[key] } },
+		).then();
 	}
 
 });
+
+// create active class
+// be able to select which list to add to
